ef4f93decde08b38c81e779c7f269a12f4c442a2
iqbaliie <miqbalmahulana@gmail.com>
2026-01-03 22:46:53 +0700

    Update ProductDetailClient.jsx

--- src/app/(shop)/[slug]/ProductDetailClient.jsx
+++ src/app/(shop)/[slug]/ProductDetailClient.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useMemo, useState } from "react";
-import { useMemo, useState } from "react";
 import { useRouter } from "next/navigation";
 import { formatDistanceToNow } from "date-fns";
 import { FaStar } from "react-icons/fa";
