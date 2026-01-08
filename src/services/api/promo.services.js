9f87203ce9edecadaf9c91e3f6e267b688aea958
presidentanjay <gentalav54@gmail.com>
2026-01-08 12:05:00 +0700

    update

--- src/services/api/promo.services.js
+++ src/services/api/promo.services.js
@@ -4,3 +4,8 @@ export async function getFlashSale() {
   const json = await getApi(`/flashsale`);
   return json?.serve ?? null;
 }
+
+export async function getSale() {
+  const json = await getApi(`/sale`);
+  return json?.serve ?? null;
+}
