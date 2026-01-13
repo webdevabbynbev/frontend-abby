f8c349fec2d5b9485adf7c8e795c8c79faa111a3
presidentanjay <gentalav54@gmail.com>
2026-01-13 15:43:21 +0700

    update
    update controller disko
    create services diskon

--- src/components/mobile/mobileBottomNav.jsx
+++ src/components/mobile/mobileBottomNav.jsx
@@ -97,7 +97,7 @@ export function MobileBottomNav({ className = "" }) {
                       profileActive ? "bg-primary-100" : null
                     )}
                   >
-                    <BtnIcon iconName="User" variant="tertiary" size="sm" />
+                    <BtnIcon as="span" iconName="User" variant="tertiary" size="sm" />
                   </div>
                   <span
                     className={clsx(
